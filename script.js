3d7fef72cd619248eb5fde86b8980d07888435d3
lavleen <lavleenbhachu4@gmail.com>
2025-11-15 17:11:10 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function glasses() {
+    var item = document.getElementById("glasses");
+    if (item.display == "block") {
+        item.display = "none";
+    } else {
+        item.display = "block";
+    }
+}
